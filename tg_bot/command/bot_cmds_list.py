7889fb2fb74877769cc2f1fb1ac754da2141dcfc
gigcomm <lenovo27835@gmail.com>
2025-01-06 23:56:53 +0300

    upd | correction name

--- tg_bot/command/bot_cmds_list.py
+++ tg_bot/command/bot_cmds_list.py
@@ -2,7 +2,7 @@ from aiogram.types import BotCommand
 
 private = [
     BotCommand(command='start', description="Запуск бота"),
-    BotCommand(command='menu', description="Посотреть меню"),
+    BotCommand(command='menu', description="Посмотреть меню"),
     BotCommand(command='about', description="О нас"),
     BotCommand(command='help', description="Помощь")
 ]
