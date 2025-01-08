7873aafab9492ff91ba48670000e2969bc524a34
gigcomm <lenovo27835@gmail.com>
2025-01-08 20:56:01 +0300

    upd: calculating the value of assets in a market

--- finance/market.py
+++ finance/market.py
@@ -16,32 +16,54 @@ class StockMarketLogic(MarketLogic):
         self.shares = shares
         self.funds = funds
 
-    def get_total_balance_shares(self):
-        return sum(share.market_price * share.quantity for share in self.shares)
+    def get_total_balance_shares_in_dollars(self):
+        result = sum(
+            (share.market_price * share.quantity) * Decimal(get_exchange_rate(share.currency, "USD")) for share in
+            self.shares)
+        return f"{result:.2f}"
 
-    def get_total_balance_funds(self):
-        return sum(fund.market_price * fund.quantity for fund in self.funds)
+    def get_total_balance_shares_in_rubls(self):
+        result =  sum(
+            (share.market_price * share.quantity) * Decimal(get_exchange_rate(share.currency, "RUB")) for share in
+            self.shares)
+        return f"{result:.2f}"
 
-    def get_total_balance_stockmarket(self):
-        return self.get_total_balance_shares() + self.get_total_balance_funds()
+    def get_total_balance_funds_in_dollars(self):
+        result =  sum((fund.market_price * fund.quantity) * Decimal(get_exchange_rate(fund.currency, "USD")) for fund in
+                   self.funds)
+        return f"{result:.2f}"
+
+    def get_total_balance_funds_in_rubls(self):
+        result =  sum((fund.market_price * fund.quantity) * Decimal(get_exchange_rate(fund.currency, "RUB")) for fund in
+                   self.funds)
+        return f"{result:.2f}"
+
+    def get_total_balance_stockmarket_in_dollars(self):
+        return Decimal(self.get_total_balance_shares_in_dollars()) + Decimal(self.get_total_balance_funds_in_dollars())
+
+    def get_total_balance_stockmarket_in_rubls(self):
+        return Decimal(self.get_total_balance_shares_in_rubls()) + Decimal(self.get_total_balance_funds_in_rubls())
+
 
 
 class ShareLogic:
-    def __init__(self, share_name, purchase_price, selling_price, market_price, quantity):
+    def __init__(self, share_name, purchase_price, selling_price, market_price, quantity, currency):
         self.share_name = share_name
         self.purchase_price = purchase_price
         self.selling_price = selling_price
         self.market_price = market_price
         self.quantity = quantity
+        self.currency = currency
 
 
 class FundLogic:
-    def __init__(self, fund_name, purchase_price, selling_price, market_price, quantity):
+    def __init__(self, fund_name, purchase_price, selling_price, market_price, quantity, currency):
         self.fund_name = fund_name
         self.purchase_price = purchase_price
         self.selling_price = selling_price
         self.market_price = market_price
         self.quantity = quantity
+        self.currency = currency
 
 
 # class BondLogic:
@@ -58,14 +80,15 @@ class CryptoMarketLogic(MarketLogic):
         self.cryptocurrencies = cryptocurrencies
 
     def get_total_balance_cryptocurrencies(self):
-        return sum(cryptocurrency.market_price * cryptocurrency.cryptocur_balance for cryptocurrency in self.cryptocurrencies)
+        return sum(
+            cryptocurrency.market_price * cryptocurrency.cryptocur_balance for cryptocurrency in self.cryptocurrencies)
 
-    def get_total_balance_cryptomarket(self):
+    def get_total_balance_cryptomarket_in_dollars(self):
         return self.get_total_balance_cryptocurrencies()
 
     def get_total_balance_cryptomarket_in_rubls(self):
         exchange_rate = Decimal(get_exchange_rate("USD", "RUB"))
-        result = self.get_total_balance_cryptomarket() * exchange_rate
+        result = self.get_total_balance_cryptomarket_in_dollars() * exchange_rate
         return f"{result:.2f}"
 
 
