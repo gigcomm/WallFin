7540a2c0c5b34d83adc3d8aae0c1312059b5cc60
gigcomm <lenovo27835@gmail.com>
2025-02-09 13:57:28 +0300

    Add config celery

--- celery_app.py
+++ celery_app.py
@@ -1,11 +1,19 @@
 from celery import Celery
 
+
 celery_app = Celery(
     "tasks",
-    broker="redis://localhost:6379/0",  # Или "amqp://guest@localhost//" для RabbitMQ
+    broker="redis://localhost:6379/0",
     backend="redis://localhost:6379/0"
 )
+celery_app.conf.timezone = "UTC"
 
-@celery_app.task
-def test_task():
-    return "✅ Celery работает!"
+import tasks.update_price_assets
+celery_app.autodiscover_tasks(["tasks"])
+
+celery_app.conf.beat_schedule = {
+    'print-every-10-seconds': {
+        'task': 'tasks.update_price_assets.test_task',
+        'schedule': 60.0,  # Запуск каждые 10 секунд
+    },
+}
