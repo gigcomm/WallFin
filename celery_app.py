9b18568451a1b3215fc58559395dcad8c82df58f
gigcomm <lenovo27835@gmail.com>
2025-02-16 18:29:58 +0300

    Upd hide data

--- celery_app.py
+++ celery_app.py
@@ -1,10 +1,15 @@
+import os
+
 from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv("tg_bot/.env")
 
 
 celery_app = Celery(
     "tasks",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker=os.getenv("REDIS_URL"),
+    backend=os.getenv("REDIS_URL")
 )
 celery_app.conf.timezone = "UTC"
 
@@ -14,6 +19,6 @@ celery_app.autodiscover_tasks(["tasks"])
 celery_app.conf.beat_schedule = {
     'print-every-10-seconds': {
         'task': 'tasks.update_price_assets.test_task',
-        'schedule': 60.0,  # Запуск каждые 10 секунд
+        'schedule': 60.0,  # Запуск каждые 60 секунд
     },
 }
