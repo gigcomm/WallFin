785ac72b103f18e22c863b313606fc46e56d93a8
gigcomm <lenovo27835@gmail.com>
2025-03-08 00:09:59 +0300

    style

--- utils/cache_utils.py
+++ utils/cache_utils.py
@@ -1,7 +1,8 @@
 import json
 
-from database.orm_query import orm_get_cryptocurrency_all, orm_get_share_all, orm_get_currency, orm_get_fund_all, \
-    orm_get_currency_all
+from database.orm_query import orm_get_cryptocurrency_all, orm_get_share_all, orm_get_fund_all, \
+    orm_get_currency_all, orm_get_cryptocurrency, orm_get_cryptocurrency_first, orm_get_share_first, orm_get_fund_first, \
+    orm_get_currency_first
 from tasks.update_price_assets import redis_client
 
 
@@ -14,18 +15,18 @@ async def get_cache_price(asset_type: str, asset_name: str, session):
 
 
     if asset_type == "cryptocurrency":
-        asset = await orm_get_cryptocurrency_all(session)
+        asset = await orm_get_cryptocurrency_first(session)
     elif asset_type == "share":
-        asset = await orm_get_share_all(session)
+        asset = await orm_get_share_first(session)
     elif asset_type == "fund":
-        asset = await orm_get_fund_all(session)
+        asset = await orm_get_fund_first(session)
     elif asset_type == "currency":
-        asset = await orm_get_currency_all(session)
+        asset = await orm_get_currency_first(session)
     else:
         return None
 
     if asset:
-        redis_client.setex(key, 60, json.dumps({"price": asset.market_price}))
+        redis_client.setex(key, 60, json.dumps({"price": float(asset.market_price)}))
         return asset.market_price
 
     return None
