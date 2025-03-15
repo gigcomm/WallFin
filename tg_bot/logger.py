cf2beb4f2c5cb1c3988c75791aa3ed0e67cb3c3c
gigcomm <lenovo27835@gmail.com>
2025-03-15 23:49:52 +0300

    Add logger

---
+++ tg_bot/logger.py
@@ -0,0 +1,14 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+handler = RotatingFileHandler(
+    "bot.log",
+    maxBytes=5 * 1024,
+    backupCount=5
+)
+handler.setFormatter(log_formatter)
+
+logger = logging.getLogger("aiogram_bot")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
