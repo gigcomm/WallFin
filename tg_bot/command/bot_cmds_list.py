5d2bfe0ffd3db409ae4b1e8b6457d8b5d72a4114
gigcomm <lenovo27835@gmail.com>
2025-06-14 18:53:33 +0300

    refactor: del commands

--- tg_bot/command/bot_cmds_list.py
+++ tg_bot/command/bot_cmds_list.py
@@ -2,7 +2,4 @@ from aiogram.types import BotCommand
 
 private = [
     BotCommand(command='start', description="Запуск бота"),
-    BotCommand(command='menu', description="Посмотреть меню"),
-    BotCommand(command='about', description="О нас"),
-    BotCommand(command='help', description="Помощь")
 ]
